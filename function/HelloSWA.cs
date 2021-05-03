0f79019c2380d24f4d83f810cba129a17d9b1a09
Julian Hüppauff <julian@hueppauff.com>
2021-05-03 11:45:47 +0200

    updated output

--- function/HelloSWA.cs
+++ function/HelloSWA.cs
@@ -18,7 +18,7 @@ namespace Function
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Welcome to Azure Static Web and Azure Functions!");
 
             return response;
         }
