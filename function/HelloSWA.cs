6dd714947c2c0f7312f48ce499801d7cf7f66eb0
Julian Hüppauff <julian@hueppauff.com>
2021-05-04 17:03:20 +0200

    downgrade .net 5 to 3.1

--- function/HelloSWA.cs
+++ function/HelloSWA.cs
@@ -1,20 +1,18 @@
 using System.Collections.Generic;
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace Function
 {
     public static class HelloSWA
     {
         [Function("HelloSWA")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-            FunctionContext executionContext)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req, ILogger log)
         {
-            var logger = executionContext.GetLogger("HelloSWA");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
             return new OkObjectResult("Welcome to Azure Static Web and Azure Functions!");
         }
