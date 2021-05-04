ebce705128a793babb4a46f51a882f51a7696541
Julian Hüppauff <julian@hueppauff.com>
2021-05-04 16:33:38 +0200

    updated to IActionResult

--- function/HelloSWA.cs
+++ function/HelloSWA.cs
@@ -3,24 +3,20 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Function
 {
     public static class HelloSWA
     {
         [Function("HelloSWA")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("HelloSWA");
             logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Static Web and Azure Functions!");
-
-            return response;
+            return new OkObjectResult("Welcome to Azure Static Web and Azure Functions!");
         }
     }
 }
