435e7b2f20d46b390f2479ff0ed6cc8ad51679f2
Julian Hüppauff <julian@hueppauff.com>
2021-05-04 21:08:19 +0200

    updated function

--- function/Main.cs
+++ function/Main.cs
@@ -1,20 +1,21 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.IO;
 using Newtonsoft.Json;
+
 
 namespace Function
 {
     public static class Main
     {
         [FunctionName(nameof(HelloSWA))]
-        public static IActionResult HelloSWA([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req, ILogger log)
+        public static IActionResult HelloSWA([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, 
+        ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
